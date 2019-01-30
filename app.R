1a2c9d5f34239bf7a215a4138b1cbc5c99ee4b5d
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2019-01-29 17:20:21 -0800

    Temporary data files for testing

--- app.R
+++ app.R
@@ -1,8 +1,18 @@
 library(shiny)
 
+# input$lat
+# input$lon
+# input$radius
+# input$routes - required to be dynamically updated based on previous inputs
+# output#map
+# output#table
+
+routes <- readcsv()
+stops <- readcsv()
+
 server <- function(input, output)
 {
-    
+    output$
 }
 
 shinyApp(ui = htmlTemplate("www/index.html"), server)
